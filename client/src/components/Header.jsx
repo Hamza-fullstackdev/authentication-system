7a46525caea6ac026995ab8dd7f751e50ed72961
Hamza Ilyas <Hamzafullstackdev1@gmail.com>
2024-09-29 00:53:45 +0500

    Complete The Home Page UI

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,8 +6,8 @@ const Header = () => {
         <div className='flex items-center justify-between'>
             <div><h3 className='text-green-400 font-bold tracking-wide text-xl'>Web <span className='text-white'>Authenticator</span></h3></div>
             <div className='flex gap-x-4 items-center'>
-                <Link className='bg-green-700 text-white text-sm outline-none border-none px-3 py-2 rounded'>Register</Link>
-                <Link className='border border-green-700 text-white text-sm px-3 py-2 rounded'>Login</Link>
+                <Link to={'/signup'} className='bg-green-700 text-white text-sm outline-none border-none px-3 py-2 rounded'>Register</Link>
+                <Link to={'/login'} className='border border-green-700 text-white text-sm px-3 py-2 rounded'>Login</Link>
             </div>
         </div>
     </header>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -8,12 +8,24 @@ const Home = () => {
         background: "url('hero-image.jpg')",
         backgroundSize: "cover",
         backgroundRepeat: "no-repeat",
-        backgroundColor: '#0000009c',
-        backgroundBlendMode:'multiply'
+        backgroundColor: "#0000009c",
+        backgroundBlendMode: "multiply",
       }}
-      className='w-100 h-[100vh]'
+      className='w-100 h-[100vh] shadow-md'
     >
       <Header />
+      <div className='text-center my-20 sm:my-40 px-2 sm:px-16'>
+        <h1 className='text-4xl sm:text-5xl text-green-400 font-bold tracking-wide'>
+          Web <span className='text-white'>Authenticator</span>
+        </h1>
+        <p className='mt-3 text-white font-semibold text-lg capitalize'>
+          Secure your account with <span className='text-green-400'>Google, GitHub, or a Passkey</span>—your choice,
+          your security!
+        </p>
+        <p className='mt-2 text-white text-md capitalize'>
+        Our web authentication app, crafted with the robust MERN stack, ensures maximum security and flexibility. Whether you prefer Google sign-up, GitHub forms, or cutting-edge passkey authentication, your data is protected with the best modern tools.
+        </p>
+      </div>
     </div>
   );
 };
